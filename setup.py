17a3b8455af26ca91be1f6801d7712337819b22e
jripoll <jripoll@lirmm.fr>
2024-10-21 22:57:48 +0200

    add setup for wrappers

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="wrappers",
+    package_dir={"": "translatome/src"},
+    packages=["wrappers"],
+    version='0.1.0',
+    description='Wrappers Utilities',
+)
